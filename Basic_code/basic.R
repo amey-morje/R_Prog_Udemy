7249137b4d281ac6b1d7ebacf5a5a0fd5dfedbe6
Amey <morje.amey@gmail.com>
2020-04-27 02:29:02 +0530

    partially done

--- Basic_code/basic.R
+++ Basic_code/basic.R
@@ -7,3 +7,17 @@ c <- "h"
  a <- c(1,86,857) #combine
  b <- seq(1,15,3) #sequence
  c <- rep("hi",5) #replicate 
+ 
+ #matrices
+ matrix()
+ cbind()
+ rbind()
+ 
+ #naming columns and rows and vectors
+ names(a) <- c("hello", "first", "c")
+ a["c"]
+ 
+ rownames() <- 
+ colnames()
+ 
+ 
